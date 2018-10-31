80ab3f258aafd566f23ad86a931e9ee74e153ab4
Alexi Gonzalez <agonzalez2016@alu.uct.cl>
2018-10-31 11:05:50 -0300

    new buttons

--- NewTideIDLE.py
+++ NewTideIDLE.py
@@ -13,11 +13,48 @@ class Boton(App):
         global btn
         wdg = Widget()
         lbl = Label()
-        btn = Button()
-        btn.text = "Archivo"
-        btn.pos = (170,560)
-        btn.size = (100, 30)
-        wdg.add_widget(btn)
+
+        btnfile = Button()
+        btnfile.text = "Archivo"
+        btnfile.pos = (170,560)
+        btnfile.size = (100, 30)
+
+        btnconex = Button()
+        btnconex.text = "Conectar"
+        btnconex.pos = (300,560)
+        btnconex.size = (100, 30)
+
+        btncomp = Button()
+        btncomp.text = "Compilar"
+        btncomp.pos = (430,560)
+        btncomp.size = (100, 30)
+
+        btnsend = Button()
+        btnsend.text = "Enviar"
+        btnsend.pos = (620,560)
+        btnsend.size = (100, 30)
+        
+        
+        wdg.add_widget(btnfile)
+        wdg.add_widget(btnconex)
+        wdg.add_widget(btncomp)
+        wdg.add_widget(btnsend)
+
+        '''
+        btnmod= Button()
+        btnmod.text = "Control/maker/Operdores"
+        btnmod.pos = (0,0)
+        btnmod.size = (200, 500)
+
+        btncod= Button()
+        btncod.text = "Arrastre aqui los modulos seleccionados"
+        btncod.pos = (300,0)
+        btncod.size = (450, 500)
+
+        wdg.add_widget(btnmod)
+        wdg.add_widget(btncod)
+        '''
+        
         lbl.markup= True
         lbl.pos = (300,300)
         lbl.font_size = "24dp"
