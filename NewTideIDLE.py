a827b8492a5691ac835bf368343ed382aeb9d93a
Alexi Gonzalez <agonzalez2016@alu.uct.cl>
2018-10-31 10:41:23 -0300

    boton
    add

--- NewTideIDLE.py
+++ NewTideIDLE.py
@@ -4,15 +4,28 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.app import App
 
-class MyApp(App):
+class Boton(App):
     def build(self):
-        parent= Widget()
-        btnstart=Button(text='Inicio')
-        btnconnect=Button(text='Conectar/Desconectar')
-        parent.add_widget(btnconnect)
-        parent.add_widget(btnstart)
-        return parent
+        global wdg
+        global lbl
+        global btn
+        wdg = Widget()
+        lbl = Label()
+        btn = Button()
+        btn.text = "Archivo"
+        btn.pos = (170,560)
+        btn.size = (100, 30)
+        wdg.add_widget(btn)
+        lbl.markup= True
+        lbl.pos = (300,300)
+        lbl.font_size = "24dp"
+        lbl.color = [1, 0.5,0.5,1]
+        wdg.add_widget(lbl)
+        return wdg
 
-if __name__== '__main__':
-    MyApp().run()
+    
+
+
+if __name__=="__main__": Boton().run()
