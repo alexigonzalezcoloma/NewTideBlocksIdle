5e7be831743af71e5ded8c508c20c8b357c552e5
Matías Bahamonde <mbahamonde2016@alu.uct.cl>
2018-11-03 19:53:46 -0300

    Update try.py
    Interfaz no funcional, con diseño "responsive" queda configurar grilla para que se muestren las áreas del tamaño indicado

--- try.py
+++ try.py
@@ -12,8 +12,9 @@ from kivy.app import App
 
 class IDLE(App):
     def build(self):
+        mainlo = GridLayout(rows=2)
         
-        layout = GridLayout(cols=5, row_force_default=True, row_default_height=40, padding=[50,50,50,50])
+        optionslo = GridLayout(cols=5, row_force_default=True, row_default_height=40, padding=[50,50,50,50])
 
         icon = Button()
         icon.text = "NewTideIDLE"
@@ -31,12 +32,42 @@ class IDLE(App):
         btnsend = Button()
         btnsend.text = "Enviar"
 
-        layout.add_widget(icon)
-        layout.add_widget(btnfile)
-        layout.add_widget(btnconex)
-        layout.add_widget(btncomp)
-        layout.add_widget(btnsend)
+        optionslo.add_widget(icon)
+        optionslo.add_widget(btnfile)
+        optionslo.add_widget(btnconex)
+        optionslo.add_widget(btncomp)
+        optionslo.add_widget(btnsend)
 
-        return layout
+        parealo = GridLayout(cols=2, padding=[10,10])
+        
+        blockslo = GridLayout(rows=2)
+        blocksopslo = GridLayout(cols=3)
+        blocksarealo = GridLayout(cols=1)
+        codearealo = GridLayout(cols=1)
+
+        btnbmaker = Button()
+        btnbmaker.text = "Maker"
+        
+        btnbcontrol = Button()
+        btnbcontrol.text = "Control"
+
+        btnboperators = Button()
+        btnboperators.text = "Operadores"
+
+        blocksopslo.add_widget(btnbmaker);blocksopslo.add_widget(btnbcontrol);blocksopslo.add_widget(btnboperators);
+        area = Image(); area.source="fondo.jpg"; area.size=(300,500)
+        blocksarealo.add_widget(area)
+
+        area2 = Image(); area2.source="fondo.jpg"; area2.size=(300,500)
+        codearealo.add_widget(area2)
+        
+        blockslo.add_widget(blocksopslo)
+        blockslo.add_widget(blocksarealo)
+        
+        parealo.add_widget(blockslo);parealo.add_widget(codearealo)
+
+        mainlo.add_widget(optionslo)
+        mainlo.add_widget(parealo)
+        return mainlo
     
 if __name__=="__main__": IDLE().run()
