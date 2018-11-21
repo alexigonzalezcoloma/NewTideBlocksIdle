4eaad06e956c09da0b7837bad1f8cb251e082a67
Alexi Gonzalez <agonzalez2016@alu.uct.cl>
2018-11-21 12:10:10 -0300

    carpetas

--- Solo IDLE/main.py
+++ Solo IDLE/main.py
@@ -13,17 +13,52 @@ from kivy.uix.filechooser import FileChooserListView
 from kivy.config import Config
 from kivy.core.window import Window
 from kivy.app import App
+import os
 
-Config.set("graphics", "height",720)
-Config.set("graphics", "width",1280)
+class IDLE(BoxLayout):
 
-class Idle(BoxLayout):
-	pass
+	def FileDialog(instance):
+	    content = BoxLayout(orientation='vertical', spacing=5)
+	    popup = Popup(title='Choose a file', content=content, size_hint=(None, None),size=(400, 400))
 
+	    path = os.getcwd()
+	    textinput = FileChooserListView(path=path, size_hint=(1, 1), dirselect=True)
+
+	    content.add_widget(textinput)
+
+	    btnlayout = BoxLayout(size_hint_y=None, height='50dp', spacing='5dp')
+	    btn = Button(text='Ok')
+	   
+	    btnlayout.add_widget(btn)
+	    btn = Button(text='Cancel')
+	    btn.bind(on_release=popup.dismiss)
+	    btnlayout.add_widget(btn)
+	    content.add_widget(btnlayout)
+	    popup.open()
+	
+	def validate(instance):
+		print("funciona")
+
+	# Abre popup para selección de puerto serial
+	def ConnectionDialog(instance):
+	    ports = list(serial.tools.list_ports.comports())
+	    content = BoxLayout(orientation='vertical', spacing=5)
+	    btnclose = Button(text='Close')
+	    content.add_widget(btnclose)
+	    popup = Popup(title='Choose a port', content=content, auto_dismiss=False, size_hint=(None, None),
+	                  size=(400, 400))
+	    btnclose.bind(on_press=popup.dismiss)
+	    popup.open()
+
+	
+
+	
+	
 class MainApp(App):
-	title ="NewTIDEIDLE"
+	title = "NEWTIDEIDLE"
 	def build(self):
-		return Idle()
-
-if __name__ == '__main__':
-	MainApp().run() 
+		self.IDLE=IDLE()
+		return self.IDLE
+		
+if __name__ == "__main__":
+	MainApp().run()
