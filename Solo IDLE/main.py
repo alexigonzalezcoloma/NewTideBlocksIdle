c074ce2b4089e1424b04e5a7344f357e8178f8b6
Alexi Gonzalez <agonzalez2016@alu.uct.cl>
2018-11-23 11:07:35 -0300

    last advance

--- Solo IDLE/main.py
+++ Solo IDLE/main.py
@@ -17,38 +17,40 @@ import os
 
 class IDLE(BoxLayout):
 
-	def FileDialog(instance):
-	    content = BoxLayout(orientation='vertical', spacing=5)
-	    popup = Popup(title='Choose a file', content=content, size_hint=(None, None),size=(400, 400))
+	def FileDialog(self):
+		print(self.ids.areamodules)
+		content = BoxLayout(orientation='vertical', spacing=5)
+		popup = Popup(title='Choose a file', content=content, size_hint=(None, None),size=(400, 400))
 
-	    path = os.getcwd()
-	    textinput = FileChooserListView(path=path, size_hint=(1, 1), dirselect=True)
+		path = os.getcwd()
+		textinput = FileChooserListView(path=path, size_hint=(1, 1), dirselect=True)
 
-	    content.add_widget(textinput)
+		content.add_widget(textinput)
 
-	    btnlayout = BoxLayout(size_hint_y=None, height='50dp', spacing='5dp')
-	    btn = Button(text='Ok')
+		btnlayout = BoxLayout(size_hint_y=None, height='50dp', spacing='5dp')
+		btn = Button(text='Ok')
 	   
-	    btnlayout.add_widget(btn)
-	    btn = Button(text='Cancel')
-	    btn.bind(on_release=popup.dismiss)
-	    btnlayout.add_widget(btn)
-	    content.add_widget(btnlayout)
-	    popup.open()
+		btnlayout.add_widget(btn)
+		btn = Button(text='Cancel')
+		btn.bind(on_release=popup.dismiss)
+		btnlayout.add_widget(btn)
+		content.add_widget(btnlayout)
+		popup.open()
 	
 	def validate(instance):
 		print("funciona")
 
+
 	# Abre popup para selección de puerto serial
 	def ConnectionDialog(instance):
-	    ports = list(serial.tools.list_ports.comports())
-	    content = BoxLayout(orientation='vertical', spacing=5)
-	    btnclose = Button(text='Close')
-	    content.add_widget(btnclose)
-	    popup = Popup(title='Choose a port', content=content, auto_dismiss=False, size_hint=(None, None),
-	                  size=(400, 400))
-	    btnclose.bind(on_press=popup.dismiss)
-	    popup.open()
+		ports = list(serial.tools.list_ports.comports())
+		content = BoxLayout(orientation='vertical', spacing=5)
+		btnclose = Button(text='Close')
+		content.add_widget(btnclose)
+		popup = Popup(title='Choose a port', content=content, auto_dismiss=False, size_hint=(None, None),
+					  size=(400, 400))
+		btnclose.bind(on_press=popup.dismiss)
+		popup.open()
 
 	
 
