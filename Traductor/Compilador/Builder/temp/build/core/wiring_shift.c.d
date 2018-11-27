d7ff1776ed9e22f97b67728cc61e35bcf09ee94e
Matías Bahamonde <mbahamonde2016@alu.uct.cl>
2018-11-27 17:53:26 -0300

    Se agregan elementos de retroalimentación
    Alertas al compilar y enviar

--- Traductor/Compilador/Builder/temp/build/core/wiring_shift.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Matias\Documents\GitHub\NewTideBlocksIdle\Traductor\Compilador\Builder\temp\build\core\wiring_shift.c.o: \
- C:\Users\Matias\Documents\GitHub\NewTideBlocksIdle\Traductor\Compilador\Builder\arduino-builder\hardware\arduino\avr\cores\arduino\wiring_shift.c \
- C:\Users\Matias\Documents\GitHub\NewTideBlocksIdle\Traductor\Compilador\Builder\arduino-builder\hardware\arduino\avr\cores\arduino\wiring_private.h \
- C:\Users\Matias\Documents\GitHub\NewTideBlocksIdle\Traductor\Compilador\Builder\arduino-builder\hardware\arduino\avr\cores\arduino\Arduino.h \
- C:\Users\Matias\Documents\GitHub\NewTideBlocksIdle\Traductor\Compilador\Builder\arduino-builder\hardware\arduino\avr\cores\arduino\binary.h \
- C:\Users\Matias\Documents\GitHub\NewTideBlocksIdle\Traductor\Compilador\Builder\arduino-builder\hardware\arduino\avr\variants\standard/pins_arduino.h
--- Traductor/Compilador/Builder/temp/build/libraries/Servo/nrf52/Servo.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Matias\Documents\GitHub\NewTideBlocksIdle\Traductor\Compilador\Builder\temp\build\libraries\Servo\nrf52\Servo.cpp.o: \
- C:\Users\Matias\Documents\GitHub\NewTideBlocksIdle\Traductor\Compilador\Builder\arduino-builder\libraries\Servo\src\nrf52\Servo.cpp
