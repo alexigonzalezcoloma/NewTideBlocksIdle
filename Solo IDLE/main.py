0e96353edaed82e77390041b8221310207a87192
Alexi Gonzalez <agonzalez2016@alu.uct.cl>
2018-11-27 01:56:15 -0300

    Add modules

--- Solo IDLE/main.py
+++ Solo IDLE/main.py
@@ -4,6 +4,7 @@ from kivy.app import App
 from kivy.graphics import Color
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
+from kivy.graphics import Color
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
@@ -18,7 +19,6 @@ import os
 class IDLE(BoxLayout):
 
 	def FileDialog(self):
-		print(self.ids.areamodules)
 		content = BoxLayout(orientation='vertical', spacing=5)
 		popup = Popup(title='Choose a file', content=content, size_hint=(None, None),size=(400, 400))
 
@@ -53,8 +53,57 @@ class IDLE(BoxLayout):
 		popup.open()
 
 	
-
 	
+	def modmakers(self):
+		areamodules=self.ids.areamodules
+		areamodules.clear_widgets()
+
+		led_on=Button(text='Encender Led')
+		led_on.Color=(1, 0, 0)
+		led_off=Button(text='Apagar Led')
+		poweron_pin=Button(text='Encender Pin')
+		poweroff_pin=Button(text='Apagar pin')
+		read_analog_pin=Button(text='Leer Pin')
+
+		areamodules.add_widget(led_on)
+		areamodules.add_widget(led_off)
+		areamodules.add_widget(poweron_pin)
+		areamodules.add_widget(poweroff_pin)
+		areamodules.add_widget(read_analog_pin)
+
+	def modcontrols(self):
+		areamodules=self.ids.areamodules
+		areamodules.clear_widgets()
+
+		control_if=Button(text='If(Condicion)')
+		control_time=Button(text='Pause(time)')
+		control_while=Button(text='While(Condicion)')
+
+		areamodules.add_widget(control_if)
+		areamodules.add_widget(control_time)
+		areamodules.add_widget(control_while)
+
+	def modopr(self):
+		areamodules=self.ids.areamodules
+		areamodules.clear_widgets()
+
+		mod_sum=Button(text='()+()')
+		mod_sum.Color=(0,1,1)
+		subtraction=Button(text='()-()')
+		multiplication=Button(text='()x()')
+		division=Button(text='()/()')
+		greater_than=Button(text='()>()')
+		smaller_than=Button(text='()<()')
+		same_that=Button(text='()==()')
+
+		areamodules.add_widget(mod_sum)
+		areamodules.add_widget(subtraction)
+		areamodules.add_widget(division)
+		areamodules.add_widget(greater_than)
+		areamodules.add_widget(smaller_than)
+		areamodules.add_widget(same_that)
+
+
 	
 class MainApp(App):
 	title = "NEWTIDEIDLE"
