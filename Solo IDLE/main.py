eed75d38c35e3fe35db3942b3276810474ca9a08
Alexi Gonzalez <agonzalez2016@alu.uct.cl>
2018-11-27 23:42:26 -0300

    cambios

--- Solo IDLE/main.py
+++ Solo IDLE/main.py
@@ -15,20 +15,16 @@ from kivy.config import Config
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.scatter import Scatter
-import os
+import os, sys; sys.path.insert(0, 'Modulo'); import Modulo;
+from Compilador import Compilador
 
 class IDLE(BoxLayout):
 
-
-        def validate(instance):
-                print("funciona")
-
-        # Muestra alertas luego de compilar o enviar
-	def Alert(ins, res):
+        def Alert(ins, res):
                 content = BoxLayout(orientation='vertical', spacing=5)
                 popup = Popup(title='Alerta', content=content, auto_dismiss=False, size_hint=(None, None), size=(480, 160))
-
-                txt = Label(text='Ha ocurrido un error')
+    
+                txt = Label(text='Ha ocurrido un error') 
                 if ins == 1:
                         if res:
                                 txt = Label(text='Se ha compilado correctamente')
@@ -42,8 +38,9 @@ class IDLE(BoxLayout):
                 btn.bind(on_release=popup.dismiss)
                 btnlayout.add_widget(btn)
                 content.add_widget(btnlayout)
-
+    
                 popup.open()
+
 
         # Invocación de compilador de arduino
         def Compilate(instance):
@@ -75,108 +72,107 @@ class IDLE(BoxLayout):
                 f.close()
                 nfh.close()
 
-
-        # Abre popup con directorio de archivos
-        def FileDialog(instance):
+        def FileDialog(self):
                 content = BoxLayout(orientation='vertical', spacing=5)
                 popup = Popup(title='Choose a file', content=content, size_hint=(None, None),size=(400, 400))
 
                 path = os.getcwd()
                 textinput = FileChooserListView(path=path, size_hint=(1, 1), dirselect=True)
-                #textinput.bind(on_path=validate)
-                #textinput = textinput
 
                 content.add_widget(textinput)
 
                 btnlayout = BoxLayout(size_hint_y=None, height='50dp', spacing='5dp')
                 btn = Button(text='Ok')
-                btn.bind(on_release=validate)
+           
                 btnlayout.add_widget(btn)
                 btn = Button(text='Cancel')
                 btn.bind(on_release=popup.dismiss)
                 btnlayout.add_widget(btn)
                 content.add_widget(btnlayout)
                 popup.open()
+        
+        def validate(instance):
+                print("funciona")
 
 
         # Abre popup para selección de puerto serial
         def ConnectionDialog(instance):
-                print(list(serial.tools.list_ports.comports()))
+                ports = list(serial.tools.list_ports.comports())
                 content = BoxLayout(orientation='vertical', spacing=5)
                 btnclose = Button(text='Close')
                 content.add_widget(btnclose)
-                popup = Popup(title='Choose a port', content=content, auto_dismiss=False, size_hint=(None, None), size=(400, 400))
+                popup = Popup(title='Choose a port', content=content, auto_dismiss=False, size_hint=(None, None),
+                                          size=(400, 400))
                 btnclose.bind(on_press=popup.dismiss)
                 popup.open()
 
+        def function_drags(self):
+                #scatter = Scatter(do_rotation=False)
+                #scatter.add_widget(subtraction)
+                print('hola')
+        
+        
+        def modmakers(self):
+                areamodules=self.ids.areamodules
+                areamodules.clear_widgets()
 
-	def function_drags(self):
-		#scatter = Scatter(do_rotation=False)
-		#scatter.add_widget(subtraction)
-		print('hola')
-	
-	def modmakers(self):
-		areamodules=self.ids.areamodules
-		areamodules.clear_widgets()
+                led_on=Button(text='Encender Led', on_press = lambda x: IDLE.fxled_on('13'))
+                led_off=Button(text='Apagar Led')
+                read_pin=Button(text='Leer Pin')
 
-		led_on=Button(text='Encender Led')
-		led_on.bind(fxled_on('13'))
-		led_off=Button(text='Apagar Led')
-		poweron_pin=Button(text='Encender Pin')
-		poweroff_pin=Button(text='Apagar pin')
-		read_analog_pin=Button(text='Leer Pin')
+                areamodules.add_widget(led_on)
+                areamodules.add_widget(led_off)
+                areamodules.add_widget(read_pin)
 
-		areamodules.add_widget(led_on)
-		areamodules.add_widget(led_off)
-		areamodules.add_widget(poweron_pin)
-		areamodules.add_widget(poweroff_pin)
-		areamodules.add_widget(read_analog_pin)
+        def fxled_on(Color):
+            nfh = open("Compilador/Builder/temp/temp.ino", "a")
+            x = Modulo.Led_On(Color)
+            nfh.write(x.write(0, ""))
+                    
 
-	def modcontrols(self):
-		areamodules=self.ids.areamodules
-		areamodules.clear_widgets()
+        
 
-		control_if=Button(text='If(Condicion)')
-		control_time=Button(text='Pause(time)')
-		control_while=Button(text='While(Condicion)')
+        def modcontrols(self):
+                areamodules=self.ids.areamodules
+                areamodules.clear_widgets()
 
-		areamodules.add_widget(control_if)
-		areamodules.add_widget(control_time)
-		areamodules.add_widget(control_while)
+                control_if=Button(text='If(Condicion)')
+                control_time=Button(text='Pause(time)')
+                control_while=Button(text='While(Condicion)')
+
+                areamodules.add_widget(control_if)
+                areamodules.add_widget(control_time)
+                areamodules.add_widget(control_while)
 
 
-	def modopr(self):
-		
-		areamodules=self.ids.areamodules
-		areamodules.clear_widgets()
-		mod_sum=Button(text='()+()')
-		mod_sum.Color=(0,1,1)
-		subtraction=Button(text='()-()')
-		multiplication=Button(text='()x()')
-		division=Button(text='()/()')
-		greater_than=Button(text='()>()')
-		smaller_than=Button(text='()<()')
-		same_that=Button(text='()==()')
+        def modopr(self):
+                
+                areamodules=self.ids.areamodules
+                areamodules.clear_widgets()
+                mod_sum=Button(text='()+()')
+                mod_sum.Color=(0,1,1)
+                subtraction=Button(text='()-()')
+                multiplication=Button(text='()x()')
+                division=Button(text='()/()')
+                greater_than=Button(text='()>()')
+                smaller_than=Button(text='()<()')
+                same_that=Button(text='()==()')
 
-		areamodules.add_widget(mod_sum)
-		areamodules.add_widget(subtraction)
-		areamodules.add_widget(division)
-		areamodules.add_widget(greater_than)
-		areamodules.add_widget(smaller_than)
-		areamodules.add_widget(same_that)
+                areamodules.add_widget(mod_sum)
+                areamodules.add_widget(subtraction)
+                areamodules.add_widget(division)
+                areamodules.add_widget(greater_than)
+                areamodules.add_widget(smaller_than)
+                areamodules.add_widget(same_that)
 
-	def fxled_on(self,n):
-	    #nfh = open("Compilador/temp/temp.ino", "a")
-	    x = Modulo.Led_On(n)
-	    x.write(0, "")
-	    #nfh.write(x.write(0, ""))
+        
 
 
 class MainApp(App):
-	title = "NEWTIDEIDLE"
-	def build(self):
-		self.IDLE=IDLE()
-		return self.IDLE
-		
+        title = "NEWTIDEIDLE"
+        def build(self):
+                self.IDLE=IDLE()
+                return self.IDLE
+                
 if __name__ == "__main__":
-	MainApp().run()
+        MainApp().run()
